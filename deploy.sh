d95a0ca06dd745f33be6025013e74c478129cec9
rohith-913 <rohith@gesain.net>
2026-01-23 17:39:39 +0530

    dep

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 set -e
-
+# Ensure cron has access to node, npm, git
 export PATH=/usr/bin:/bin:/usr/local/bin
-
+# Move to project root
 cd /var/www/astro-test/scorecard_cms
-
 echo "---- DEPLOY START $(date) ----"
-
-git pull origin main
+# Always deploy exactly what is on GitHub
+git checkout main
+git fetch origin
+git reset --hard origin/main
+git clean -fd
+# Install dependencies
 npm install
+# Run custom script (if required)
 node scripts/updateMethodology.js
+# Build Astro (static build)
 npm run build
-
 echo "---- DEPLOY END $(date) ----"
-
